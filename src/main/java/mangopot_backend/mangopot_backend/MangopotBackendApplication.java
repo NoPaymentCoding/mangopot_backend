766a40da4cd1bf7d9d2c5a69774d46def69ab08d
YujeongLEE <dldbwjd24@ewhain.net>
2023-01-06 16:37:14 +0900

    feat : spring setting

---
+++ src/main/java/mangopot_backend/mangopot_backend/MangopotBackendApplication.java
@@ -0,0 +1,13 @@
+package mangopot_backend.mangopot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangopotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangopotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mangopot_backend/mangopot_backend/MangopotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mangopot_backend.mangopot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangopotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
