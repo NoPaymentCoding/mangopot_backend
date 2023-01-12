989610540cd2c5641c575816f9afd8f327884aa8
LimGayoung <ygy920@gmail.com>
2023-01-12 15:21:47 +0900

    ci: update deploy.sh
    add -f

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ then
 else
   echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
   sudo docker stop $CURRENT_PID
-  sudo docker system prune -a && sudo y
+  sudo docker system prune -a -f
   sleep 5
 fi
 
